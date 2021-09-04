c2c726e81dd5a7b71d252afb418849dba8ae4fb3
marwacoder <49866172+marwacoder@users.noreply.github.com>
2021-09-04 02:06:31 +0100

    touchings

--- src/views/AuthDashboard/Dashboard.js
+++ src/views/AuthDashboard/Dashboard.js
@@ -10,11 +10,15 @@ export default function Dashboard() {
             <Box className='centered'>
       <img style={{width: 300, height: 300, opacity: 0.5 }}  src={ellipse} alt='ellipse'/>
     </Box>
+    <Box textAlign='center' left={'30%'} fontWeight='bold' bottom={'10%'} position='fixed' color='red'>IT IS AN OFFENSE PURNISHABLE BY LAW IN THE FEDERAL REPUBLIC OF NIGERIA IF INSTRUMENTS USED FOR TRADE ARE NOT REGISTERED WITH THE WEIGHTS AND MEASURES DEPARTMENT</Box>
+
       </Hidden>
        
     <Hidden smUp>
     <Box className='centered'>
       <img style={{width: 200, height: 200, opacity: 0.5 }}  src={ellipse} alt='ellipse'/>
+      <Box color='red'>IT IS AN OFFENSE PURNISHABLE BY LAW IN THE FEDERAL REPUBLIC OF NIGERIA IF INSTRUMENTS USED FOR TRADE ARE NOT REGISTERED WITH THE WEIGHTS AND MEASURES DEPARTMENT</Box>
+
     </Box>
     </Hidden>
     
